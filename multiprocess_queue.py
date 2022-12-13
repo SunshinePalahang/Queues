0a3b9f605707a2a191523330d2f2a2c6620afd8a
Sunshine Richmer Palahang <sunshinerichmerdpalahang@iskolarngbayan.pup.edu.ph>
2022-12-13 12:20:29 +0800

    Removing reversed_md5 function

--- multiprocess_queue.py
+++ multiprocess_queue.py
@@ -61,17 +61,17 @@ def chunk_indices(length, num_chunks):
         length -= chunk_size
         num_chunks -=1
 
-def reverse_md5(hash_value, alphabet=ascii_lowercase, max_length=6):
-    for length in range (1, max_length+1):
-        for combination in Combinations(alphabet, length):
-            text_bytes = "".join(combination).encode("utf-8")
-            hashed = md5(text_bytes).hexdigest()
-            if hashed == hash_value:
-                return text_bytes.decode("utf-8")
+# def reverse_md5(hash_value, alphabet=ascii_lowercase, max_length=6):
+#     for length in range (1, max_length+1):
+#         for combination in Combinations(alphabet, length):
+#             text_bytes = "".join(combination).encode("utf-8")
+#             hashed = md5(text_bytes).hexdigest()
+#             if hashed == hash_value:
+#                 return text_bytes.decode("utf-8")
 
 def main():
     t1 = time.perf_counter()
-    text =  reverse_md5("a9d1cbf71942327e98b40cf5ef38a960")
+    # text =  reverse_md5("a9d1cbf71942327e98b40cf5ef38a960")
     print(f"{text}(found in {time.perf_counter()-  t1:.1f}s)")
 
 if __name__ == "__main__":
