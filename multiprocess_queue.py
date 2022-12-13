a8129b26daf8f94e73e9ba919509aef91f54cce4
Sunshine Richmer Palahang <sunshinerichmerdpalahang@iskolarngbayan.pup.edu.ph>
2022-12-13 13:43:18 +0800

    Implementing while loop

--- multiprocess_queue.py
+++ multiprocess_queue.py
@@ -1,6 +1,8 @@
 import time
 import multiprocessing
 import argparse
+import queue
+import time
 from hashlib import md5
 from string import ascii_lowercase
 from dataclasses import dataclass
@@ -87,6 +89,19 @@ def main(args):
         combinations = Combinations(ascii_lowercase, text_length)
         for indices in chunk_indices(len(combinations), len(workers)):
             queue_in.put(Job(combinations, *indices))
+
+    while any(worker.is_alive() for worker in workers):
+        try:
+            solution = queue_out.get(timeout=0.1)
+            if solution:
+                t2 = time.perf_counter()
+                print(f"{solution}(found in {t2-t1:.1f}s)")
+                break
+        except queue.Empty:
+            pass
+        else:
+            print("Unable to find a solution")
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
